7a6f416047e0b82f3ab12e2e4f7a98d20c9a4227
williamsjs <williamsjs1224@gmail.com>
2017-10-16 20:50:28 -0400

    remove join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017004413) do
+ActiveRecord::Schema.define(version: 20171017003659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
