ae9369deb141271d389128aeafab1eb5852e0f14
williamsjs <williamsjs1224@gmail.com>
2017-10-19 20:57:38 -0400

    add update, destroy, create to author controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-  before_action :set_author, only: [:show]
+  before_action :set_author, only: [:show, :update, :destroy]
 
   def index
     @authors = Author.all
@@ -11,8 +11,34 @@ class AuthorsController < ApplicationController
     render json: @author
   end
 
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      render json: @author, status: :created, location: @author
+    else
+      render json: @author.errors, status: :unprocessable_entity      
+    end
+  end
+
+  def update
+    if @author.update(author_params)
+      render json: @author
+    else
+      render json: @author.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @author.destroy
+  end
+
   private
     def set_author
       @author = Author.find(params[:id])
+    end
+
+    def author_params
+      params.require(:author).permit(:name, :bio, :date_of_birth)
     end
 end
